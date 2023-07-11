067af36aed0a4f522e1067670fe8a3fcf928637a
smoonmare <smoonmare@gmail.com>
2023-07-10 17:55:53 -0700

    Refactor(style): Minor changes to analytics service and details component
    This commit introduces some minor style-related changes, primarily focusing on the 'analytics.service.ts' and 'details.component.ts' files. It also includes commented code in 'main.ts' that adds a provider for the AnalyticsService.
    
    Key Changes:
    
    - Adjusted spacing in the @Injectable decorator in 'analytics.service.ts' for consistency.
    - Removed trailing comma from 'styleUrls' array in 'details.component.ts' to adhere to style guidelines.
    - Commented-out code in 'main.ts' demonstrates how to add providers when bootstrapping the application, specifically for the AnalyticsService.
    
    These changes do not affect functionality but improve consistency and provide an example for further service integration.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+// import { AnalyticsService } from './app/shared/analytics.service';
 
 
 if (environment.production) {
   enableProdMode();
 }
+
+// bootstrapApplication(AppComponent, {
+//   providers: [AnalyticsService]
+// });
 
 bootstrapApplication(AppComponent);
